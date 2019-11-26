d171bef3fbe6345b6cc4d57c7ce89bc04f1a7680
Melonfruity <eric.luo24@gmail.com>
2019-11-26 23:02:16 +0000

    recursive sum function mk1

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if(fromN !== toN){
+    return sum(fromN + 1, toN) + fromN;
+  }
+  return 0;
 }
 
 module.exports = sum;
